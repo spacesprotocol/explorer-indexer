9e6aa49ff5edf67b2599b9bd3f57c6cf9a510a3a
Alexander Shevtsov <randomlogin76@gmail.com>
2025-10-14 17:26:03 +0200

    removed goose statement declaration

--- sql/schema/20251014171355_add_mempool_index.sql
+++ sql/schema/20251014171355_add_mempool_index.sql
@@ -1,13 +1,9 @@
--- +goose NO TRANSACTION
 -- +goose Up
--- +goose StatementBegin
+-- +goose NO TRANSACTION
 SET statement_timeout = 0;
 CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_transactions_mempool
 ON transactions(block_hash)
 WHERE block_hash = '\xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef';
--- +goose StatementEnd
 
 -- +goose Down
--- +goose StatementBegin
 DROP INDEX CONCURRENTLY IF EXISTS idx_transactions_mempool;
--- +goose StatementEnd
